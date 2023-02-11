fbb7e4086258fb6d7ba4eb6852fd204169d4621d
kgonzalez92o <KennethGonzalez26@gmail.com>
2023-02-11 11:47:13 -0600

    Ejercicios de clase semana 3
    Se agrega el ejercicio de la semana 3

---
+++ Clase3/resources/js/logica.js
@@ -0,0 +1,29 @@
+//declaracion es gyuardar un pequeño espacio en la memoria temporal 
+//Variable: espacio de memoria que puede cambiar su contenido a lo largo de la ejecucion.
+//Var indica una virable global 
+var title;
+// let indica que es variable estructural
+let subtitulo;
+//constante : espacio en moria que nunca cambia 
+const height = 200;
+
+
+//inicializar es dar valor a un espacio de memoria
+title = "Este es mi primer ejercicio de javascript"
+subtitulo = "Suerte!";
+
+
+//JS es un lenguaje no tipado
+//string texto
+//number = numerico
+//array = lista datos
+//boolean = verdadero o falso
+
+List = ["manzana", "peras", "sandia"]
+
+//Objetos es la representacion de un elemento del mundo real
+
+var user ={
+    name: 'Kenneth',
+    lastname: 'Gonzalez'
+}
